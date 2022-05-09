dea6206119dbc52d5ad7b774f3a9ee32251744c2
tns410 <60863616+tns410@users.noreply.github.com>
2022-05-09 13:48:56 +0200

    Correct mistake

--- source/board_test.h
+++ source/board_test.h
@@ -6,9 +6,7 @@ void pulsewidth_test(void);
 void servo_test(void);
 void throttle_test(void);
 void test_pixy(void);
-void set_servo(double 
 void set_servo(double servo);
-void set_speed(double speed););
 void set_speed(double speed);
 
 
